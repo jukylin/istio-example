5bc614e1d55cb1b15e6ca8b15e6ffd8f8e6ea536
742161455@qq.com <742161455@qq.com>
2018-09-10 00:21:58 +0800

    test

--- php-swoole/index.php
+++ php-swoole/index.php
@@ -3,7 +3,7 @@ $http = new swoole_http_server("127.0.0.1", 8000);
 $http->on('request', function ($request, $response) {
     $json = json_encode($request);
     echo $json;
-    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+    $response->end("<h1>Hello Swoole. $json </h1>");
 });
 $http->start();
 
